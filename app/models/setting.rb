e09c3e4b256d28e171153706d8d719beebf1fac2
MasudaAfrin <masudatuba@gmail.com>
2023-09-13 04:33:18 +0600

    categories and price options methods

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -1,2 +1,23 @@
 class Setting < ApplicationRecord
+  def self.product_categories
+    categories = Setting.first.app_setting['categories']
+    categories.map.with_index do |cat, index|
+      {
+        id: index + 1,
+        value: cat,
+        label: cat
+      }
+    end
+  end
+
+  def self.price_options
+    price_options = Setting.first.app_setting['rent_buy_options']
+    price_options.map.with_index do |option, index|
+      {
+        id: index + 1,
+        value: option,
+        label: option
+      }
+    end
+  end
 end
