d8f7f263dcf580604d400a9c89d8a23ec5bce7a4
MasudaAfrin <masudatuba@gmail.com>
2023-09-13 03:28:45 +0600

    user phone number validation fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,14 @@ class User < ApplicationRecord
   has_secure_token :auth_token, length: 30
 
   validates :phone_number, uniqueness: true, presence: true,
-            format: { with: /\+[0-9]+/,
+            format: { with: /[0-9]{10}/,
                       message: 'Please provide valid phone number'}
   validates :email, uniqueness: true, presence: true,
             format: { with: URI::MailTo::EMAIL_REGEXP,
                       message: 'Please provide valid email' }
   validates :first_name, :last_name, presence: true
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
 end
