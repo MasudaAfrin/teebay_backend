95b03531397411f3584c0fa8f17da7f4e4a09d74
MasudaAfrin <masudatuba@gmail.com>
2023-09-13 03:28:06 +0600

    login and user create api path

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  namespace :api do
+    namespace :v1 do
+      post '/login', to: 'session#create'
+      resources :users, only: [:create]
+    end
+  end
 end
