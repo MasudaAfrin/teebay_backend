97c5e62e7aba4e06ad7f30ac714170f228f6c1fb
MasudaAfrin <masudatuba@gmail.com>
2023-09-13 02:21:36 +0600

    validation added in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ApplicationRecord
+  has_secure_password validations: false
+  has_secure_token :auth_token, length: 30
+
+  validates :phone_number, uniqueness: true, presence: true,
+            format: { with: /\+[0-9]+/,
+                      message: 'Please provide valid phone number'}
+  validates :email, uniqueness: true, presence: true,
+            format: { with: URI::MailTo::EMAIL_REGEXP,
+                      message: 'Please provide valid email' }
+  validates :first_name, :last_name, presence: true
 end
